3af5620fd9962bca79d142cc0386dce55f8c8a7b
Melody <mlmusmacker@gmail.com>
2024-11-03 13:32:04 -0700

    Update ui_generator.py
    Created generate_row helper function,

--- ui_generator.py
+++ ui_generator.py
@@ -16,8 +16,14 @@ def generate_ui(rows):
         ui_block.append(generate_row(row,max_width))
     return ui_block
 
-def generate_row():
-    pass
+def generate_row(row,width):
+    column_width = width // len(row)
+    output = "|"
+    for column in row:
+        output += generate_column(column,column_width)
+    if width % len(row) != 0:
+        output = output[:-1] + " |"
 
-def generate_column():
+
+def generate_column(column):
     pass
