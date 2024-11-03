906a3b85f545af3b013c8501a6c91e4e50d756bd
Melody <mlmusmacker@gmail.com>
2024-11-03 13:31:46 -0700

    Update ui_generator.py
    Adjusted generate_ui function to match use case.

--- ui_generator.py
+++ ui_generator.py
@@ -5,18 +5,16 @@
 def generate_ui(rows):
     max_width = 0
     for row in rows:
-        if isinstance(row, str) and len(row) + 4 > max_width:
-            max_width = len(row) + 3
-        else:
-            current_width = 0
-            for column in row:
-                  current_width = len(column) + 3
-            if current_width > max_width:
-                max_width = current_width
+        current_width = 0
+        for column in row:
+                current_width = len(column["content"]) + 3
+        if current_width > max_width:
+            max_width = current_width
     
     ui_block = []
     for row in rows:
-        ui_block.append(generate_row(row))
+        ui_block.append(generate_row(row,max_width))
+    return ui_block
 
 def generate_row():
     pass
